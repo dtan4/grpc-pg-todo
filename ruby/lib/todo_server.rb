afe7e7bd209584ff07ecb325ce399045680b7f8b
Daisuke Fujita <dtanshi45@gmail.com>
2016-05-31 19:29:05 +0900

    Modify server implementation

--- ruby/lib/todo_server.rb
+++ ruby/lib/todo_server.rb
@@ -1,17 +1,34 @@
+require "logger"
+require "todo_services"
+
+module RubyLogger
+  def logger
+    LOGGER
+  end
+
+  LOGGER = Logger.new(STDOUT)
+end
+
+module GRPC
+  extend RubyLogger
+end
+
 class TodoServer < Todo::Service
   def initialize(conn)
     @conn = conn
   end
 
-  def add_task(task)
-    conn.exec("INSERT INTO todos(title, description) VALUES($1, $2)", [task.title, task.description])
+  def add_task(task, _call)
+    @conn.exec("INSERT INTO todos(title, description) VALUES($1, $2)", [task.title, task.description])
     task
   end
 
-  def list_tasks(list_request)
-    conn.query("SELECT title, description FROM todos").inject([]) do |todos, column|
-      todos << Todo.new(title: column["title"], description: column["description"])
-      todos
+  def list_tasks(list_request, _call)
+    tasks = @conn.query("SELECT title, description FROM todos").inject([]) do |result, column|
+      result << Task.new(title: column["title"], description: column["description"])
+      result
     end
+
+    Tasks.new(tasks: tasks)
   end
 end
